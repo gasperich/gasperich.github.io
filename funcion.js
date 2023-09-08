30a0c32d47dfa978b5596f6158565208cb5dacf4
gasperich <84154441+gasperich@users.noreply.github.com>
2023-09-08 13:31:15 +0200

    Add files via upload

--- funcion.js
+++ funcion.js
@@ -1,3 +1,14 @@
 
+/*
+const botoncito = document.getElementsByClassName("botoncito")[0]
+const secciones = document.getElementsByClassName("secciones")[0]
+
+botoncito.addEventListener('click', ( ) => {
+    secciones.classList.toggle('active')
+})
 
 
+function DisplaySecciones() {  
+    document.getElementsByClassName("botoncito").innerHTML = "secciones"}
+
+*/
